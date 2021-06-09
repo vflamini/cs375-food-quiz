ecdeaac71355623a080abb876b990bd9a44a7e28
Bunfay <59625801+Bunfay@users.noreply.github.com>
2021-06-09 13:03:43 -0400

    Update index.js
    Populate DB

--- index.js
+++ index.js
@@ -43,6 +43,60 @@ app.post('/merchants', (req, res) => {
 })
 */
 
+restaurantList = [
+  ["Italian","La Fontana Della Citta","https://www.lafontanadellacitta.com/"],
+  ["Italian","Maggiano's Little Italy","https://www.maggianos.com/?campaign=brand&gclid=CjwKCAjwnPOEBhA0EiwA609RefGTSZKmSKJk48WC4mscbyreolT6JDmIUn_F6Zu2HfbbrWSFuVLs2hoC0PkQAvD_BwE"],
+  ["Italian","Osteria","https://www.osteriaphilly.com/"],
+  ["Italian","Giorgio On Pine","http://www.giorgioonpine.com/"],
+  ["Italian","Pietro's Italian","https://pietrospizza.com/"],
+  ["Mexican","Distrito","https://www.distritophilly.com/"],
+  ["Mexican","Rosy's Taco Bar","https://rosystacobar.com/"],
+  ["Mexican","Mission Taqueria","https://www.missiontaqueria.com/"],
+  ["Mexican","El Vez","https://elvezrestaurant.com/?utm_source=Google%20My%20Business&utm_medium=Website%20Button"],
+  ["Mexican","El Ray","https://elreyrestaurant.com/?utm_source=Google%20My%20Business&utm_medium=Website%20Button"],
+  ["Chinese","Han Dynasty","https://handynasty.net/"],
+  ["Chinese","Sang Kee Noodle House","https://sangkeenoodlehouse.com/"],
+  ["Chinese","Dim Sum & Noodle","https://www.dimsumandnoodle.com/"],
+  ["Chinese","Mandarin Palace","https://www.phillymandarinpalace.com/?utm_source=gmb&utm_medium=website"],
+  ["Chinese","Nom Wah ","https://nomwah.com/"],
+  ["Indian","Tiffin Indian Cuisine","https://order.tiffin.com/"],
+  ["Indian","Veda","https://vedaphilly.com/"],
+  ["Indian","Ekta Indian Cuisine","https://www.ektaindianrestaurant.com/"],
+  ["Indian","Ateethi","http://ateethirestaurantpa.com/"],
+  ["Indian","Thanal Indian Tavern","https://www.thanalphilly.com/"],
+  ["American","New Deck Tavern","https://www.newdecktavern.com/"],
+  ["American","White Dog Cafe","https://whitedog.com/location/?University-City-1"],
+  ["American","Butcher Bar","http://www.butcherbarphilly.com/"],
+  ["American","Yards Brewing Company","https://yardsbrewing.com/"],
+  ["American","The Love.","https://theloverestaurant.com/?utm_source=Google%20My%20Business&utm_medium=Website%20Button"],
+  ["Healthy","The Quick Fixx","https://thequickfixx.com/"],
+  ["Healthy","P.S. & Co.","https://www.puresweets.com/"],
+  ["Healthy","Just Salad","https://www.justsalad.com/"],
+  ["Healthy","Freshii","https://www.freshii.com/ca/en-ca/home"],
+  ["Healthy","Crisp Kitchen","https://www.crispkitchen.com/"],
+  ["Sushi","Crazy Shushi","https://www.phillycrazysushi.com/"],
+  ["Sushi","Morimoto","https://morimotorestaurant.com/"],
+  ["Sushi","Pod","https://podrestaurant.com/?utm_source=Google%20My%20Business&utm_medium=Website%20Button"],
+  ["Sushi","Zama","http://www.zamaphilly.com/"],
+  ["Sushi","Double Knot","https://www.doubleknotphilly.com/"]
+];
+
+pool.query(`SELECT EXISTS (SELECT 1 FROM restaurants)`,(err,res)=>{
+  if (res == null){
+    pool.query(`CREATE TABLE restaurants (cuisine VARCHAR(25), name VARCHAR(25), website VARCHAR(250))`,(err,res)=>{
+      console.log(err,res)
+    });
+  }
+});
+
+for(var i = 0; i < restaurantList.length; i++) {
+  pool.query(
+    `INSERT INTO restaurants(cuisine, name, website) 
+    VALUES($1, $2, $3)
+    RETURNING *`,
+    [restaurantList[i][0], restaurantList[i][1], restaurantList[i][2]]
+)};
+
 app.post("/restaurants", function (req, res) {
     let body = req.body;
     
@@ -61,61 +115,15 @@ app.post("/restaurants", function (req, res) {
         res.status(500);
         res.send();
     });
-});
-
-app.get("/droptable", function (req, res) {
-  let body = req.body;
-  
-  pool.query(
-      `DROP TABLE restaurants`
-  ).then(function (response) {
-      res.send()
-      res.status(200)
-  })
-  .catch(function (error) {
-      console.log(error);
-      res.status(500);
-      res.send();
-  });
-});
-
-app.get("/createtable", function (req, res) {
-  let body = req.body;
-  
-  pool.query(
-      `CREATE TABLE restaurants(cuisine VARCHAR(75),name VARCHAR(75),website VARCHAR(75))`
-  ).then(function (response) {
-      res.send()
-      res.status(200)
-  })
-  .catch(function (error) {
-      console.log(error);
-      res.status(500);
-      res.send();
-  });
-});
-
-app.get("/tableexists", function (req, res) {
-  let body = req.body;
-  
-  pool.query(
-      `SELECT to_regclass('public.restaurants');`
-  ).then(function (response) {
-      res.send(response.rows)
-      res.status(200)
-  })
-  .catch(function (error) {
-      console.log(error);
-      res.status(500);
-      res.send();
-  });
+    console.log(body);
+    res.send();
 });
 
 app.get("/", function (req, res) {
-
+    // TODO extract species from query
     let cuisine = req.query.cuisine;
     let rows = null;
-
+    // TODO run query selecting all animals with species
     pool.query(
         `SELECT * FROM restaurants WHERE cuisine = $1`,
         [cuisine]
@@ -131,7 +139,8 @@ app.get("/", function (req, res) {
         res.status(500);
         res.send();
     });
-
+    // TODO return rows to user
+    //res.send(rows);
 });
 
 app.listen(port, () => {
